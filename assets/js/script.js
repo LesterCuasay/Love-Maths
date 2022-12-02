51a664e66220526c5f7fcae104f8e91998f4fe8f
LesterCuasay <lestercuasay@live.co.uk>
2022-12-02 15:33:58 +0000

    add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     rungame("addition");
 })
 function rungame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
